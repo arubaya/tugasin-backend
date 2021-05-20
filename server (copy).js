f42123792edbc259621e0607a0d7a429f4ff525e
arubaya <abi.arubaya@gmail.com>
2021-05-20 09:58:26 +0700

    make server run on https self signed

--- server (copy).js
+++ server (copy).js
@@ -18,23 +18,8 @@
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
-const https = require('https')
-const pem = require('pem')
 
-pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
-  if (error) {
-    return console.log(error)
-  }
-
-  const options = {
-    key: keys.serviceKey,
-    cert: keys.certificate
-  }
-
-  new Ignitor(require('@adonisjs/fold'))
-    .appRoot(__dirname)
-    .fireHttpServer((handler) => {
-      return https.createServer(options, handler)
-    })
-    .catch(console.error)
-})
+new Ignitor(require('@adonisjs/fold'))
+  .appRoot(__dirname)
+  .fireHttpServer()
+  .catch(console.error)
